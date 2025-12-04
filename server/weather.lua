4835300088658285aee52ff8187bd597a895caa4
0Programmer <60280452+0Programmer@users.noreply.github.com>
2025-12-04 16:54:33 +0100

    feat(playerBlackOut): Allow toggle server wide or player only blackouts

--- server/weather.lua
+++ server/weather.lua
@@ -90,6 +90,22 @@ lib.addCommand('blackout', {
     GlobalState.blackOut = not GlobalState.blackOut
 end)
 
+lib.addCommand('blackout', {
+    help = 'Toggle server wide or player only blackout',
+	restricted = 'group.admin',
+	params = {
+		{ name = 'target', type = 'playerId', help = 'Target player\'s server id', optional = true },
+	}
+}, function(source, args)
+	if not args.target then
+		GlobalState.blackOut = not GlobalState.blackOut
+	else
+		local playerState = Player(args.target)
+        if not playerState then return end
+        playerState.state:set('playerBlackOut', not playerState.state?.playerBlackOut, true)
+	end
+end)
+
 -- Scheduled restart --
 if useScheduledWeather then
     AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
